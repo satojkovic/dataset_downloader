a448236755b54aa9e6be13c20e30e84f64a46919
satojkovic <satojkovic@gmail.com>
2017-10-12 22:09:41 +0900

    Add a new method

--- download_flickr8k.py
+++ download_flickr8k.py
@@ -3,10 +3,25 @@
 
 import requests
 from bs4 import BeautifulSoup
+import os
+import urllib
+import re
+
+BASE_URL = 'http://nlp.cs.illinois.edu/HockenmaierGroup/8k-pictures.html'
+OUTPUT_DIR = 'imgs'
+
+
+def get_image(url):
+    pass
 
 
 def main():
-    pass
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+
+    for image_url, image_name in get_image(BASE_URL):
+        urllib.request.urlretrieve(image_url,
+                                   os.path.join(OUTPUT_DIR, image_name))
 
 
 if __name__ == '__main__':
